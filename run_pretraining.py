5c0316386b085930d627e57407cf7a39caf6fc46
Niketan Pansare <niketan_pansare@apple.com>
2021-01-09 21:21:20 -0800

    Switched from map_and_batch_with_legacy_function to map_and_batch

--- run_pretraining.py
+++ run_pretraining.py
@@ -442,7 +442,7 @@ def input_fn_builder(input_files,
     # and we *don't* want to drop the remainder, otherwise we wont cover
     # every sample.
     d = d.apply(
-        tf.data.experimental.map_and_batch_with_legacy_function(
+        tf.data.experimental.map_and_batch( #map_and_batch_with_legacy_function(
             lambda record: _decode_record(record, name_to_features),
             batch_size=batch_size,
             num_parallel_batches=num_cpu_threads,
